aa1d7bf49cc1bf4927ede72c7dbd36564e0c4e2a
kbhatt23 <kanishklikesprogramming@gmail.com>
2020-11-10 00:10:12 +0530

    clinic service using jpa

---
+++ clinic-appointment-service/src/main/java/com/learning/jpa/clinic_appointment_service/ClinicAppointmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learning.jpa.clinic_appointment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicAppointmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicAppointmentServiceApplication.class, args);
+	}
+
+}
